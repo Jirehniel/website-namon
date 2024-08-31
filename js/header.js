ba34db121cc893c20416464b1907faf2c3dad168
xoxolrjj <xwonzws@gmail.com>
2024-08-31 19:33:38 +0800

    qwerty

---
+++ js/header.js
@@ -0,0 +1,11 @@
+document.write(`
+    <header class="navbar">
+        <img src="images/logo.jpg" class="logo" alt="Logo">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about_us.html">About us</a></li>
+            <li><a href="gallery.html">Explore Beaches</a></li>
+            <li><a href="contact_us.html">Contact Us</a></li>
+        </ul>
+    </header>
+`);
