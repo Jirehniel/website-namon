5e0c9dcc8eab65208e5a7be6c9c625bdac271f3b
xoxolrjj <xwonzws@gmail.com>
2024-09-03 11:21:00 +0800

    ez

--- js/footer.js
+++ js/footer.js
@@ -8,9 +8,9 @@
             <a href="mailto:jirehandmissythebeach@gmail.com">jirehandmissythebeach@gmail.com</a>
         </p>
          <div class="social_media">
-                <a href="https://web.facebook.com/100074338364998/videos/636349364392825?idorvanity=891650874686036" target="_blank"><img src="images/facebook.png" alt="Facebook"></a>
-                <a href="https://x.com/?lang=en" target="_blank"><img src="images/twitter.png" alt="Twitter"></a>
-                <a href="https://www.instagram.com" target="_blank"><img src="images/instagram.png" alt="Instagram"></a>
+                <a href="https://web.facebook.com/100074338364998/videos/636349364392825?idorvanity=891650874686036" target="_blank"><img src="../images/facebook.png" alt="Facebook"></a>
+                <a href="https://x.com/?lang=en" target="_blank"><img src="../images/twitter.png" alt="Twitter"></a>
+                <a href="https://www.instagram.com" target="_blank"><img src="../images/instagram.png" alt="Instagram"></a>
             </div>
         </div>
         </div>
--- js/header.js
+++ js/header.js
@@ -1,11 +1,11 @@
 document.write(`
     <header class="navbar">
-        <img src="images/logo.jpg" class="logo" alt="Logo">
+        <img src="../images/logo.jpg" class="logo" alt="Logo">
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about_us.html">About us</a></li>
-            <li><a href="gallery.html">Explore Beaches</a></li>
-            <li><a href="contact_us.html">Contact Us</a></li>
+            <li><a href="../pages/index.html">Home</a></li>
+            <li><a href="../pages/about_us.html">About us</a></li>
+            <li><a href="../pages/gallery.html">Explore Beaches</a></li>
+            <li><a href="../pages/contact_us.html">Contact Us</a></li>
         </ul>
     </header>
 `);
