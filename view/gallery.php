58ebda2b447ac275a962eff1c2783d85ae111c7b
Jirehniel <144858861+Jirehniel@users.noreply.github.com>
2024-09-19 02:07:24 +0800

    missy love goku

--- view/gallery.php
+++ view/gallery.php
@@ -27,7 +27,9 @@
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">El Nido Beach</div>
+                <div class="img_profile">
                 <img src="images/el_nido.jpg">
+                </div>
                 <p>A haven for divers and beach lovers alike, El Nido is renowned for its limestone cliffs and vibrant marine life. Located in El Nido, Palawan, MIMAROPA. 
                 <a href="https://www.google.com/maps/place/El+Nido,+Palawan/@11.1955845,119.4219206,12z/data=!3m1!4b1!4m6!3m5!1s0x33b11f65f6d68a3d:0x5eb1b6a6f2dbf14!8m2!3d11.1956142!4d119.4387103!16s%2Fg%2F11c6_b0ls5" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/ElnidoPalawanPhilippines" target="_blank">Visit on Facebook</a></p>
@@ -36,14 +38,18 @@
         <ul class="gallery_list">
             <li class="gallery_item">
                 <div class="gallery_title">Palawan Beach</div>
+                <div class="img_profile">
                 <img src="images/palawan.jpg">
+                </div>
                 <p>Often considered the best island in the world, Palawan offers stunning seascapes and hidden lagoons. Located in Puerto Princesa, Palawan, MIMAROPA. 
                 <a href="https://www.google.com/maps/place/Puerto+Princesa,+Palawan/@9.7458355,118.8262753,12z/data=!3m1!4b1!4m6!3m5!1s0x33b3fd9600575899:0x4b28dc3fd83f622!8m2!3d9.7458368!4d118.8245392!16s%2Fg%2F11b6l6c5zd" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/groups/2118469938175986" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Siargao Beach</div>
+                <div class="img_profile">
                 <img src="images/siargao.jpg">
+                </div>
                 <p>The surfing capital of the Philippines, Siargao is famous for its laid-back vibe and impressive waves. Located in General Luna, Siargao Island, Surigao del Norte, Caraga. 
                 <a href="https://www.google.com/maps/place/General+Luna,+Siargao+Island,+Surigao+del+Norte/@9.826222,125.9597868,12z/data=!3m1!4b1!4m6!3m5!1s0x32fd752e59b569ff:0x3801bb4f7f9d7b3!8m2!3d9.8262215!4d125.9749795!16s%2Fg%2F11c3ksdxbq" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/groups/798877421323411" target="_blank">Visit on Facebook</a></p>
@@ -52,14 +58,18 @@
         <ul class="gallery_list">
             <li class="gallery_item">
                 <div class="gallery_title">Bohol Beach</div>
+                <div class="img_profile">
                 <img src="images/bohol.jpg">
+                </div>
                 <p>Home to the Chocolate Hills and beautiful beaches, Bohol is a diverse destination with much to offer. Located in Panglao Island, Bohol, Central Visayas. 
                 <a href="https://www.google.com/maps/place/Panglao,+Bohol/@9.5824925,123.8253282,12z/data=!3m1!4b1!4m6!3m5!1s0x33b94e24c6b9a773:0x721b4a488c2c9c3!8m2!3d9.582468!4d123.8406602!16s%2Fg%2F11c4jz74_2" target="_blank">View on Google Maps</a> | 
                 <a href="https://www.facebook.com/boholbeachclubresort" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Cebu Beach</div>
+                <div class="img_profile">
                 <img src="images/cebu.jpg">
+                </div>
                 <p>Known for its historical significance and stunning beaches, Cebu is a blend of culture and natural beauty. Located in Mactan Island, Cebu, Central Visayas. 
                 <a href="https://www.google.com/maps/place/Mactan/@10.288069,123.9840078,13z/data=!3m1!4b1!4m6!3m5!1s0x33a99a061bc9db91:0xe15c27a22fde07e8!8m2!3d10.2990463!4d123.9638531!16zL20vMDJmazhk?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/profile.php?id=100094175805697" target="_blank">Visit on Facebook</a></p>
@@ -68,14 +78,18 @@
         <ul class="gallery_list">
             <li class="gallery_item">
                 <div class="gallery_title">Coron Beach</div>
+                <div class="img_profile">
                 <img src="images/coron.jpg">
+                </div>
                 <p>A diver's paradise, Coron is famous for its underwater shipwrecks and pristine coral reefs. Located in Coron, Palawan, MIMAROPA. 
                 <a href="https://www.google.com/maps/place/Coron,+Palawan/@12.0180743,120.214634,10z/data=!3m1!4b1!4m6!3m5!1s0x33ba1ef225b0d675:0x68841ab9a85e968e!8m2!3d12.0489179!4d120.1519011!16zL20vMDJocW5k?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/groups/1189906664974839" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Pagudpud Beach</div>
+                <div class="img_profile">
                 <img src="images/pagudpud.jpg">
+                </div>
                 <p>Often referred to as the "Boracay of the North," Pagudpud offers unspoiled beaches and majestic views. Located in Pagudpud, Ilocos Norte, Ilocos Region. 
                 <a href="https://www.google.com/maps/place/Pagudpud,+Ilocos+Norte/@18.565733,120.8766085,12z/data=!3m1!4b1!4m6!3m5!1s0x338927491180a9df:0xb5f6f482da8bac79!8m2!3d18.5874677!4d120.8220827!16zL20vMDVnOTd5?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/Pagudpud.Saud.White.Beach" target="_blank">Visit on Facebook</a></p>
@@ -84,14 +98,18 @@
         <ul class="gallery_list">
             <li class="gallery_item">
                 <div class="gallery_title">Camiguin Beach</div>
+                <div class="img_profile">
                 <img src="images/camiguin.jpg">
+                </div>
                 <p>A volcanic island with hot springs, waterfalls, and stunning beaches, Camiguin is an island of adventures. Located in Mambajao, Camiguin, Northern Mindanao. 
                 <a href="https://www.google.com/maps/place/Mambajao,+Camiguin/@9.2175047,124.706583,13z/data=!3m1!4b1!4m6!3m5!1s0x3300655e1266df27:0xd001fb3b01275f21!8m2!3d9.2321675!4d124.7077665!16zL20vMDZteF8z?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/groups/597140380630906" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Samal Beach</div>
+                <div class="img_profile">
                 <img src="images/samal.jpg">
+                </div>
                 <p>Just off the coast of Davao, Samal Island boasts pristine beaches and a rich marine ecosystem. Located in Island Garden City of Samal, Davao del Norte, Davao Region. 
                 <a href="https://www.google.com/maps/place/Island+Garden+City+of+Samal,+Davao+del+Norte/@7.0452462,125.7300405,11z/data=!3m1!4b1!4m6!3m5!1s0x32f9701aad3fe53b:0xebd6a17f6716630f!8m2!3d7.103312!4d125.7188463!16zL20vMDZqcmpi?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/samalbeachtravel" target="_blank">Visit on Facebook</a></p>
@@ -100,14 +118,18 @@
         <ul class="gallery_list">
             <li class="gallery_item">
                 <div class="gallery_title">Kalanggaman Island</div>
+                <div class="img_profile">
                 <img src="images/kalanggaman.jpg">
+                </div>
                 <p>Known for its long stretch of sandbar, Kalanggaman is a hidden gem in Leyte. Located in Palompon, Leyte, Eastern Visayas. 
                 <a href="https://www.google.com/maps/place/Palompon,+Leyte/@11.2842517,124.1460421,12z/data=!3m1!4b1!4m6!3m5!1s0x33bb0ec89d1dc9d1:0xd1485486b07a676!8m2!3d11.2841152!4d124.160175!16s%2Fg%2F11c6z8n6h2" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/kalanggaman.island.tours" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Anawangin Beach</div>
+                <div class="img_profile">
                 <img src="images/ananawan.jpg">
+                </div>
                 <p>A crescent-shaped beach with a backdrop of pine trees, Anawangin Cove is perfect for camping and beachcombing. Located in San Antonio, Zambales, Central Luzon. 
                 <a href="https://www.google.com/maps/place/San+Antonio,+Zambales/@14.8574943,120.0877064,12z/data=!3m1!4b1!4m6!3m5!1s0x3395d6d8fced80cb:0xab2814638d2a7545!8m2!3d14.9275093!4d120.0896313!16zL20vMDJsMnRr?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/groups/1862810807150675" target="_blank">Visit on Facebook</a></p>
@@ -116,21 +138,27 @@
         <ul class="gallery_list">
             <li class="gallery_item">
                 <div class="gallery_title">Panglao Island</div>
+                <div class="img_profile">
                 <img src="images/panglao.jpg">
+                </div>
                 <p>Part of the Bohol province, Panglao is famous for its stunning white-sand beaches and world-class diving spots. Located in Panglao, Bohol, Central Visayas. 
                 <a href="https://www.google.com/maps/place/Panglao,+Bohol/@9.5822138,123.763175,13z/data=!3m1!4b1!4m6!3m5!1s0x33abace2adb22c61:0xbf332f18bfdc0d41!8m2!3d9.5731429!4d123.7629465!16zL20vMDZsZ3h0?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/alonabeach.panglao.bohol.philippines" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Malapascua Island</div>
+                <div class="img_profile">
                 <img src="images/malapascua.jpg">
+                </div>
                 <p>Known for its thresher sharks, Malapascua offers an unforgettable underwater experience. Located in Daanbantayan, Cebu, Central Visayas. 
                 <a href="https://www.google.com/maps/place/Daanbantayan,+Cebu/@11.3343444,124.0410304,11z/data=!3m1!4b1!4m6!3m5!1s0x33a876d368e67095:0x38b19758a6433451!8m2!3d11.2464955!4d124.0173086!16zL20vMDZoNGQy?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/MalapascuaBeachandDiveResort" target="_blank">Visit on Facebook</a></p>
             </li>
             <li class="gallery_item">
                 <div class="gallery_title">Cagbalete Island</div>
+                <div class="img_profile">
                 <img src="images/cagbalete.jpg">
+                </div>
                 <p>A serene island in Quezon Province, Cagbalete is perfect for those seeking peace and quiet in a natural setting. Located in Mauban, Quezon, CALABARZON. 
                 <a href="https://www.google.com/maps/place/Mauban,+Quezon/@14.2499833,121.7214774,11z/data=!3m1!4b1!4m6!3m5!1s0x33980745ceb9df57:0x5006e19779479fa3!8m2!3d14.1807733!4d121.7062391!16zL20vMDZyYnRf?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D" target="_blank">View on Google Maps</a> | 
                 <a href="https://web.facebook.com/pansacolabeachresort" target="_blank">Visit on Facebook</a></p>
